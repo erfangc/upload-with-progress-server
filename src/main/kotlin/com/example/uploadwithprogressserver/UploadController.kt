eef6efb7fd2eea0546deccc790ece97dd149db2a
Erfang Chen <erfangc@gmail.com>
2023-01-07 21:17:01 -0500

    initial commit

--- src/main/kotlin/com/example/uploadwithprogressserver/UploadController.kt
+++ src/main/kotlin/com/example/uploadwithprogressserver/UploadController.kt
@@ -1,53 +1,22 @@
 package com.example.uploadwithprogressserver
 
 import org.slf4j.LoggerFactory
-import org.springframework.http.HttpHeaders
-import org.springframework.http.ResponseEntity
-import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.CrossOrigin
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestMethod
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.multipart.MultipartFile
 
-data class UploadResponse(val message: String)
-
-@Controller
+@RestController
+@CrossOrigin
 class UploadController {
     private val log = LoggerFactory.getLogger(UploadController::class.java)
 
-    @RequestMapping(
-        value = ["upload"],
-        method = [RequestMethod.POST],
-        consumes = ["multipart/form-data"],
-        produces = ["application/json"],
-    )
-    fun upload(@RequestParam file: MultipartFile): ResponseEntity<UploadResponse> {
+    @PostMapping(value = ["upload"])
+    fun upload(@RequestParam file: MultipartFile): UploadResponse {
         log.info(
-            "Uploading file bytes=${file.bytes} " +
-                    "contentType=${file.contentType} " +
-                    "size=${file.size} " +
-                    "originalFilename=${file.originalFilename} "
+            "Uploading file bytes=${file.bytes} " + "contentType=${file.contentType} " + "size=${file.size} " + "originalFilename=${file.originalFilename} "
         )
-        return ResponseEntity
-            .status(200)
-            .header(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "*")
-            .header(HttpHeaders.ACCESS_CONTROL_ALLOW_HEADERS, "*")
-            .header(HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS, "*")
-            .body(UploadResponse(message = "Ok"))
+        return UploadResponse(message = "Ok")
     }
-     @RequestMapping(
-        value = ["upload"],
-        method = [RequestMethod.OPTIONS],
-        consumes = ["*/*"],
-    )
-    fun uploadPreflight(): ResponseEntity<String> {
-        return ResponseEntity
-            .status(200)
-            .header(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "*")
-            .header(HttpHeaders.ACCESS_CONTROL_ALLOW_HEADERS, "*")
-            .header(HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS, "*")
-            .body("Ok")
-    }
-    
 }
---
+++ src/main/kotlin/com/example/uploadwithprogressserver/UploadResponse.kt
@@ -0,0 +1,3 @@
+package com.example.uploadwithprogressserver
+
+data class UploadResponse(val message: String)
